83d2188f2d68b4e3cdb9587870438ac9741e4038
Murat Akdeniz <info@webfikirleri.com>
2021-09-17 16:45:14 +0300

    Create WConfig.php

---
+++ Models/WConfig.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace WConfig\Models;
+
+use CodeIgniter\Model;
+
+class WConfig extends Model
+{
+	protected $DBGroup              = 'default';
+	protected $table                = 'wconfig';
+	protected $primaryKey           = 'id';
+	protected $useAutoIncrement     = true;
+	protected $insertID             = 0;
+	protected $returnType           = 'object';
+	protected $useSoftDeletes       = false;
+	protected $protectFields        = true;
+	protected $allowedFields        = ['name','code','lang_code','input_type','input_values','value','group'];
+
+	// Dates
+	protected $useTimestamps        = true;
+	protected $dateFormat           = 'datetime';
+	protected $createdField         = 'created_at';
+	protected $updatedField         = 'updated_at';
+	protected $deletedField         = 'deleted_at';
+
+	// Validation
+	protected $validationRules      = [];
+	protected $validationMessages   = [];
+	protected $skipValidation       = false;
+	protected $cleanValidationRules = true;
+
+	// Callbacks
+	protected $allowCallbacks       = true;
+	protected $beforeInsert         = [];
+	protected $afterInsert          = [];
+	protected $beforeUpdate         = [];
+	protected $afterUpdate          = [];
+	protected $beforeFind           = [];
+	protected $afterFind            = [];
+	protected $beforeDelete         = [];
+	protected $afterDelete          = [];
+
+}
